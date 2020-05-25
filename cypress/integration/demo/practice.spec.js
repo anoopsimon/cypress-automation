2dfe21a3b4d6b115dea1a0622151f1c30f421ae1
Anoop Simon <anoop.simon@adudev.com>
2020-05-25 17:25:35 +1000

    added one more test

--- cypress/integration/demo/practice.spec.js
+++ cypress/integration/demo/practice.spec.js
@@ -23,6 +23,14 @@ context('Actions', () => {
       cy.get('.product-name').should('contain', 'Faded Short Sleeve T-shirts')
     })
   
+    it('Verify Login to APP', () => {
+      cy
+      .get('.login')
+      .click()
+
+      cy.get('#SubmitLogin').click()
+      cy.get('.alert-danger').should('contain', 'An email address required.')
+    })
     
   })
   
