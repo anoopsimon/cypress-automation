a62bbc332a6e626b4f675902a01e3112cb396d0d
Anoop Simon <anoop.simon@adudev.com>
2020-05-27 13:45:31 +1000

    upadated readme with screenshot

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A simple Javascript project to demonstrate power of Cypress
 * HTML Report with screenshots - Using mochawesome
 * Configuration Driven Tests - Using cypress.json
 * Test cases to demonstrate browser interactions using cypress, assertions etc.
+
+ Mocha Awesome Report - Passed
+![](cypress\docs\mochawesome_passed.gif)
+
+ Mocha Awesome Report - Failed With Screenshots
+ ![](cypress\docs\mochawesome_passed.gif)
--- cypress/integration/automationpractice/practice.spec.js
+++ cypress/integration/automationpractice/practice.spec.js
@@ -3,34 +3,30 @@ context('Actions', () => {
     beforeEach(() => {
       cy.visit('/')
     })  
-  
-    it('Verify Search in Automation Practice', () => {
-      cy
-      .get('#search_query_top')
-      .type('shirt')
+    
 
-      cy.get('button[name="submit_search"]').click()
-      cy.get('.product-name').should('contain', 'Faded Short Sleeve T-shirts')
-
-    })
-
-    it('Verify Search Result', () => {
-      cy
-      .get('#search_query_top')
-      .type('shirt')
+    it('Validate that search returns result', () => {
+      cy.get('#search_query_top').type('shirt')
 
       cy.get('button[name="submit_search"]').click()
       cy.get('.product-name').should('contain', 'Faded Short Sleeve T-shirts')
     })
   
-    it('Verify Login to APP', () => {
-      cy
-      .get('.login')
-      .click()
-
+    it('Validate that email is mandatory for login', () => {
+      cy.get('.login').click()
       cy.get('#SubmitLogin').click()
       cy.get('.alert-danger').should('contain', 'An email address required.')
     })
+
+    
+    it('Validate that password is mandatory for login', () => {
+      const email='testuser@gmail.com'
+      cy.get('.login').click()
+      cy.get('#email').type(email).should('have.value', email)
+      cy.get('#SubmitLogin').click()
+      cy.get('.alert-danger').should('contain', 'Password is required.')
+    })
+    
     
   })
   
