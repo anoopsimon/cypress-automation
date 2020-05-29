3f18fbf0ed1e976fb2dab4ec338a74d78649976d
Anoop Simon <anoop.simon@adudev.com>
2020-05-29 15:52:36 +1000

    implemented read fixture file using fixture keyword

--- cypress/integration/automationpractice/search.spec.js
+++ cypress/integration/automationpractice/search.spec.js
@@ -1,10 +1,17 @@
 context('Automation practice - Search Tests',() =>{
     beforeEach(()=>{
         cy.visit('/')
+        cy.fixture('searchdata').then((data) => {
+            searchTestData = data
+          })
+    
     })
 
+    let searchTestData
+
+  
     it('Search should return results',()=> {        
-        const searchTerm='Shirt'
+        let searchTerm = searchTestData.searchKeyword
         cy.enterText('#search_query_top',searchTerm)
         cy.clickElement('button[name="submit_search"]')        
         cy.assertContainsText('.product-name', 'Faded Short Sleeve T-shirts')
